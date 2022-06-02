f9bca8f78c3e02c02aa1137896fd3bcf36177879
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-02 23:06:55 +0430

    create render html file

--- website/views.py
+++ website/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
 def index_view(request):
-    return HttpResponse('<h1>index</h1>')
+    return render(request,'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request,'website/index.html')
 
 
 def contact_view(request):
-    return HttpResponse('<h1>contact</h1>')
+    return render(request,'website/index.html')
