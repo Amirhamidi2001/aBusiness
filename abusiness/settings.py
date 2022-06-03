df693fabffb1a74dd9c967b99834ed6fcaf9bc6b
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-03 20:37:20 +0430

    Update settings.py

--- abusiness/settings.py
+++ abusiness/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-02fpy2s+5naetdjf2bu1%j+&_6do5#9qk$v3^&ioyn_c)bank7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abusiness.herokuapp.com']
 
 
 # Application definition
@@ -117,14 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR/ 'assets')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'assets'
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
