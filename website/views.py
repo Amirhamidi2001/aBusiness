65dabb480ffba769c9f780d8093394f2fc479ac2
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-03 15:26:53 +0430

    Update urls

--- website/views.py
+++ website/views.py
@@ -7,8 +7,16 @@ def index_view(request):
 
 
 def about_view(request):
-    return render(request,'website/index.html')
+    return render(request,'website/about.html')
 
 
 def contact_view(request):
-    return render(request,'website/index.html')
+    return render(request,'website/contact.html')
+
+
+def portfolio_view(request):
+    return render(request,'website/portfolio.html')
+
+
+def services_view(request):
+    return render(request,'website/services.html')
