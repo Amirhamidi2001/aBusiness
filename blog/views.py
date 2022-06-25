798887fac007d2575dab52ba98476d9e89184708
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-25 11:12:45 +0430

    Update project

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from os import stat
-from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 # Create your views here.
 
-def blog_view(request, cat_name = None):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name:
-        posts = posts.filter(category__name = cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name = kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -16,3 +16,12 @@ def blog_single(request,pid):
     post = get_object_or_404(Post,pk=pid)
     context = {'post': post}
     return render(request, 'blog/blog-single.html',context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if request.GET.get('s'):
+            s =  request.GET.get('s')
+            posts = posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
