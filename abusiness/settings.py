c0aa1aef630684f6cba9bdf4b4642b78cde1dd1c
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-06-27 08:52:10 +0430

    Update settings.py

--- abusiness/settings.py
+++ abusiness/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,5 +135,4 @@ MEDIA_ROOT = 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
 django_heroku.settings(locals())
