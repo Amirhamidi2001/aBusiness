38de6b263d3020452862488fb2fc4b8684eee1b0
Amirhamidi2001 <Amirarsalanh2001@gmail.com>
2022-07-01 09:57:14 +0430

    Update source code

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,10 +3,12 @@ from blog.models import Post, Category
 
 register = template.Library()
 
+
 @register.inclusion_tag('blog/blog-recent-post.html')
 def post_recent():
     posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
     return {'posts': posts}
+
 
 @register.inclusion_tag('blog/blog-categories.html')
 def postcategory():
@@ -15,4 +17,4 @@ def postcategory():
     cat_dict = {}
     for name in catgories:
         cat_dict[name] = posts.filter(category=name).count()
-    return {'category':cat_dict}
+    return {'category': cat_dict}
--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -3,6 +3,7 @@ from blog.models import Post
 
 register = template.Library()
 
+
 @register.inclusion_tag('website/website-latest-news.html')
 def post_recent():
     posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
